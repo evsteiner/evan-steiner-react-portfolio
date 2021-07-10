b9fc278d6a29ff51c404af9b78917219faad80f7
evsteiner <evstein.ut@gmail.com>
2021-07-10 09:34:28 -0600

    'SecondCommit'

---
+++ src/components/pages/about.js
@@ -0,0 +1,37 @@
+import React from "react";
+import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
+
+export default function() {
+    return (
+        <div className="content-page-wrapper">
+            <div
+                className="left-column"
+                style={{
+                    background: "url(" + profilePicture + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                }}
+            />
+            <div className="right-column">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur corporis qui nesciunt nobis est beatae modi quaerat maxime unde perspiciatis ipsum, inventore, voluptatum omnis neque nulla at iusto harum optio.
+                Alias, nesciunt explicabo veritatis voluptatem dignissimos ducimus culpa? Animi incidunt magni soluta laudantium, iusto accusamus, modi repudiandae exercitationem sed cupiditate, iste optio debitis eius id. Soluta quod expedita beatae praesentium?
+                Expedita minima atque placeat officiis quas quam laboriosam ab, doloremque dignissimos obcaecati eligendi ullam minus repudiandae a eos adipisci sequi at. Laudantium minus assumenda illum provident id aspernatur minima sed!
+                Quisquam libero beatae omnis? Doloremque incidunt suscipit, modi error nam veritatis facere accusamus quae impedit cupiditate in omnis, obcaecati officia est molestias sed neque corporis nisi vero reprehenderit nesciunt labore!
+                Nemo qui consequuntur error quo nulla molestiae eveniet asperiores neque corporis eum fugiat excepturi eius, vero esse culpa voluptate laborum quia. Obcaecati, blanditiis? Praesentium ipsum, explicabo illum vel ab id!
+                In, eum eius ut nostrum, cum iure, modi dolor quia illum rem architecto. Porro maiores ipsam aliquid quasi laborum quaerat cumque deserunt et praesentium adipisci, odio harum itaque, delectus dignissimos.
+                Debitis quidem ipsum nihil minus exercitationem cupiditate cum minima, quod architecto adipisci repudiandae ipsa distinctio, voluptatem ducimus quos! Veniam provident, quidem ut ipsa omnis perspiciatis qui aliquam iste voluptate incidunt!
+                Harum excepturi repellat ex at architecto quo suscipit facilis nulla assumenda, qui earum expedita ab delectus maiores doloremque impedit fugit sequi voluptatum nihil quas. Dolorem incidunt ad blanditiis itaque deleniti.
+                Quasi, esse, eaque, omnis nisi repudiandae laudantium nihil adipisci qui voluptatibus earum dolorum eligendi praesentium. Fugiat sint ullam necessitatibus excepturi accusantium, expedita impedit ipsa tempora ex vero totam aspernatur vel.
+                Molestiae cum mollitia impedit! Obcaecati ipsam aliquam aperiam commodi maiores inventore totam incidunt officiis esse vel, consequuntur cum amet quaerat necessitatibus illo enim eos earum molestiae neque dolore voluptatibus? Quibusdam!
+                Esse alias fuga placeat accusantium quasi eveniet et numquam inventore qui cupiditate dolorum, error facilis vel delectus molestiae ducimus necessitatibus similique. Earum distinctio nemo animi itaque repellat dolores beatae quas!
+                Animi exercitationem quas hic nemo dolorem illo a vel vitae iste ipsum reiciendis, saepe error ipsa suscipit nesciunt, quidem tempora dignissimos nostrum sunt deserunt explicabo culpa rem? Iste, voluptatibus ex?
+                Rerum provident qui similique veritatis id sequi aspernatur, repellat esse eligendi deserunt. Voluptates accusantium alias debitis natus ut aut officiis quam facere, fuga veniam reiciendis, atque aspernatur, repudiandae repellat eveniet.
+                Ea error facere autem incidunt! Dolores similique accusamus quia quaerat dolore error soluta molestiae, eligendi in obcaecati nihil iste veniam totam repudiandae unde perferendis. Ad tempora ratione provident rem alias?
+                Consequatur incidunt, possimus officia architecto illum doloremque consequuntur amet? Expedita magnam optio aliquam doloribus deleniti quaerat sed tempore aperiam non. Reprehenderit deleniti nisi qui blanditiis quis, illum itaque eum laboriosam?
+                Debitis odit itaque sit fugit dolore quasi natus praesentium doloremque quod dolorum nulla tempore quaerat numquam, tenetur veniam hic velit blanditiis eveniet veritatis. Molestiae earum magni eos eveniet esse qui!
+                Aspernatur voluptatem, quas debitis nemo amet consectetur suscipit deserunt dolor ad illum molestias quidem earum at soluta perferendis eum sapiente itaque ipsum nam. Iure, sunt vero. Neque aliquam quo aspernatur.
+                Quidem, asperiores impedit vel ab repellendus quae neque, quam esse iusto minus aliquid explicabo nobis. Distinctio, laborum eligendi. Cumque ducimus voluptates atque sequi nulla, facere possimus aperiam. Suscipit, laudantium et?
+            </div>
+        </div>
+    );
+}
